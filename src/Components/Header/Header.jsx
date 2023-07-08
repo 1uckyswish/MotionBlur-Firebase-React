bfbaf450209a9951e88000ccceca69b6dc00a9a1
Noel Guillen <noelblas274@gmail.com>
2023-07-08 14:30:59 -0400

    header styles completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,13 @@ import './Header.css';
 function Header() {
   return (
     <header>
-        <h1>MotionBl<span>ur</span></h1>
+        <h1>Motion</h1>
+        <div className='trending-hashtags'>
+          <p>Trending</p>
+          <p>Music</p>
+          <p>Photos</p>
+          <p>Profile</p>
+        </div>
         <button>Sign Up</button>
     </header>
   )
