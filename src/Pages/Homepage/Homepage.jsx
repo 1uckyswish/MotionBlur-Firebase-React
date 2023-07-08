808564c3e59792d24e2e79cc7c6ada13f007ddf8
Noel Guillen <noelblas274@gmail.com>
2023-07-08 19:57:49 -0400

    header images and slider done

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import './Homepage.css'
+import React, { useState, useEffect } from 'react';
+import './Homepage.css';
+import { Banner } from '/src/Utils/data';
 
 function Homepage() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (index >= Banner.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    }, 7000);
+  }, [index]);
+
+  const bannerStyles = {
+    backgroundImage: `url("${Banner[index].image}")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'right',
+    backgroundRepeat: 'no-repeat',
+    height: '42vh',
+    opacity: 1,
+    transition: 'background-image 20s ease-in-out background-position 9s ease-in-out, opacity 0.5s ease-in-out',
+    backdropFilter: 'blur(5px)',
+    backgroundColor: 'black',
+    // filter: 'brightness(80%)',
+  };
+
   return (
     <section>
-     Homepage
+      <div className='banner' style={bannerStyles}>
+        <div className='banner-header'>
+          <h2>Unleash Your Creativity with Motion Blurr</h2>
+          <p>Share, Explore, & Capture Moments in Motion</p></div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
---
+++ src/Utils/data.js
@@ -0,0 +1,41 @@
+import Image1 from "../assets/bannerImgs/IMG1.jpg";
+import Image2 from "../assets/bannerImgs/IMG2.jpg";
+import Image3 from "../assets/bannerImgs/IMG3.jpg";
+import Image4 from "../assets/bannerImgs/IMG4.jpg";
+import Image5 from "../assets/bannerImgs/IMG5.jpg";
+import Image6 from "../assets/bannerImgs/IMG6.jpg";
+import Image7 from "../assets/bannerImgs/IMG7.jpg";
+import Image8 from "../assets/bannerImgs/IMG8.jpg";
+import Image9 from "../assets/bannerImgs/IMG9.jpg";
+import Image10 from "../assets/bannerImgs/IMG10.jpg";
+import Image11 from "../assets/bannerImgs/IMG11.jpg";
+import Image12 from "../assets/bannerImgs/IMG12.jpg";
+import Image13 from "../assets/bannerImgs/IMG13.jpg";
+import Image14 from "../assets/bannerImgs/IMG14.jpg";
+import Image15 from "../assets/bannerImgs/IMG15.jpg";
+import Image16 from "../assets/bannerImgs/IMG16.jpg";
+import Image17 from "../assets/bannerImgs/IMG17.jpg";
+import Image18 from "../assets/bannerImgs/IMG18.jpg";
+import Image19 from "../assets/bannerImgs/IMG19.jpg";
+
+export const Banner = [
+  { id: 1, image: Image1 },
+  { id: 2, image: Image2 },
+  { id: 3, image: Image3 },
+  { id: 4, image: Image4 },
+  { id: 5, image: Image5 },
+  { id: 6, image: Image6 },
+  { id: 7, image: Image7 },
+  { id: 8, image: Image8 },
+  { id: 9, image: Image9 },
+  { id: 10, image: Image10 },
+  { id: 11, image: Image11 },
+  { id: 12, image: Image12 },
+  { id: 13, image: Image13 },
+  { id: 14, image: Image14 },
+  { id: 15, image: Image15 },
+  { id: 16, image: Image16 },
+  { id: 17, image: Image17 },
+  { id: 18, image: Image18 },
+  { id: 19, image: Image19 }
+];
