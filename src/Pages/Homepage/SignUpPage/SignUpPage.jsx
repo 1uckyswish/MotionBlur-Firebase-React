448e2ec79d9de1139e295ce2e7b130da99468b92
Noel Guillen <noelblas274@gmail.com>
2023-07-09 19:50:28 -0400

    finished static styles for signup

--- src/Pages/Homepage/SignUpPage/SignUpPage.jsx
+++ src/Pages/Homepage/SignUpPage/SignUpPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SignUpPage.css'
 
 function SignUpPage() {
+    const [signIn, setSignIn] = useState(true);
+
   return (
-    <div>SignUpPage</div>
+    <div className='form-container'>
+        <div className='form-box-container'>
+            <div className={signIn? "box-1" : "welcome-member"}>
+            </div>
+            <div className="box-2">
+                {
+                signIn ? (
+                        <>
+                        <h2>Create Account</h2>
+                        <h4>Your Journey Starts Here: Sign Up for Limitless Possibilities!</h4>
+                        </>) 
+                        : (
+                        <>
+                        <h2>Welcome Back</h2>
+                        <h4>Continue Your Journey of Limitless Possibilities!</h4>
+                        </>)
+                }
+                    <form>
+                        <label>Username</label>
+                        <input type='text' placeholder='Username'/>
+                        <label>Email Address</label>
+                        <input type='email' placeholder='Email'/>
+                        <label>Password</label>
+                        <input type='password' placeholder='Password'/>
+                        <input type="button" value='Submit' />
+                    </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
