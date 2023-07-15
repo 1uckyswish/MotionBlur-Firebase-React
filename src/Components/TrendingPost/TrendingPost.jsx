c35b02e0d7d36a84f942e955e22bfc6a13e22518
Noel Guillen <noelblas274@gmail.com>
2023-07-15 19:25:32 -0400

    trending page applied dummy data

---
+++ src/Components/TrendingPost/TrendingPost.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiOutlineComment, AiOutlineHeart, AiFillHeart} from "react-icons/ai";
+
+
+function TrendingPost({post}) {
+    const [liked, SetLiked] = useState(false);
+    const [followed, setFollowed] = useState(false);
+  return (
+    <div className='music-container'>
+        <div className='music-header'>
+            <img src='https://images.unsplash.com/photo-1551847812-f815b31ae67c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80' alt='profileImg'/>
+                <div className='music-info'>
+                    <h3>Din Djarin</h3>
+                    <p>July 5th 2023</p>
+                </div>
+        </div>
+        <div className='music-caption'>
+            <p>Stepping into the realm of my all-time favorite song, where every note is a masterpiece and every lyric holds a special place in my heart. The melody, the lyrics, and the emotions intertwine to create an extraordinary experience that never fails to captivate me. Sharing a piece of my musical passion with you all. 🎶✨ #FavoriteSong #MusicEnthusiast #MelodicMasterpiece #SongMagic #UnforgettableMoments #TimelessClassic #MemorableLyrics #IncredibleComposition</p>
+        </div>
+        <div className="music-image">
+           <div class="ratio ratio-16x9">
+            <iframe width="560" height="315" src={post} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        </div>
+        <div className="music-social">
+            <div className='social-icons'>
+                {
+                    liked?
+                    <AiFillHeart id='liked-icon' onClick={()=> SetLiked(!liked)}/>
+                    :
+                    <AiOutlineHeart onClick={()=> SetLiked(!liked)}/>
+                }
+                <AiOutlineComment/>
+            </div>
+            {
+                followed?
+                 <p id='follow-button' onClick={()=> setFollowed(!followed)}>Unfollow</p>
+                 :
+                <p onClick={()=> setFollowed(!followed)}>Follow</p>
+            }
+        </div>
+    </div>
+  );
+}
+
+export default TrendingPost
--- src/Pages/Trending/Trending.jsx
+++ src/Pages/Trending/Trending.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import './Trending.css'
+import React from 'react';
+import './Trending.css';
+import TrendingPost from '../../Components/TrendingPost/TrendingPost';
+import SinglePost from '../../Components/SinglePost/SinglePost';
 
 function Trending() {
-   const links = [
-  'https://www.youtube.com/embed/TOGswC4X8Nc?start=278',
-  'https://www.youtube.com/embed/hc9EBvwkSzY?start=1',
-  'https://www.youtube.com/embed/COz9lDCFHjw?start=59',
-  'https://www.youtube.com/embed/H5v3kku4y6Q?start=14'
+ const links = [
+  "https://www.youtube.com/embed/9bZkp7q19f0?start=69",
+  "https://images.unsplash.com/photo-1688383213635-9bc2fc25103d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://www.youtube.com/embed/tG35R8F2j8k?start=69",
+  "https://images.unsplash.com/photo-1600832501609-0ba64370e7ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+  "https://images.unsplash.com/photo-1536009744269-d24508b32196?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://images.unsplash.com/photo-1587279484404-29792a7c375c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://www.youtube.com/embed/76O3w4pt0CA?start=69",
+  "https://images.unsplash.com/photo-1536009744269-d24508b32196?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  "https://www.youtube.com/embed/Czs2B53M444?start=69"
 ];
 
 
+  function isYouTubeLink(link) {
+    return link.includes('youtube.com');
+  }
+
   return (
     <div className='music-page-container'>
-    <h2>Trending</h2>
-    {
-        links.map((item)=>{
-           return <p>Hi</p>
-        })
-    }
+      <h2>Trending</h2>
+      {links.map((item) => {
+        if (isYouTubeLink(item)) {
+          return <TrendingPost post={item} />;
+        } else {
+          return <SinglePost image={item} />;
+        }
+      })}
     </div>
-  )
+  );
 }
 
-export default Trending
+export default Trending;
