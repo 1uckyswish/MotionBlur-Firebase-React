3d7584c3fe33c233faeec9b30ce6fb2cee8c8371
Noel Guillen <noelblas274@gmail.com>
2023-07-24 22:25:50 -0400

    added firestore exports for database

--- src/Pages/AddPost/AddPost.jsx
+++ src/Pages/AddPost/AddPost.jsx
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from 'react';
 import './AddPost.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactPlayer from 'react-player';
+import {ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+//make sure the user is logged in
+import { useAuthState } from 'react-firebase-hooks/auth';
+// get the firestore exports from the config file
+import { storage, db, auth } from '../../Config/FirebaseConfig';
+import {v4} from 'uuid';
 
 function AddPost() {
   const [mediaType, setMediaType] = useState(false);
@@ -14,7 +20,6 @@ function AddPost() {
     PostType: '',
     MediaUrl: '',
   });
-  console.log(formData)
 
   useEffect(() => {
     if (selectedImage) {
@@ -22,33 +27,51 @@ function AddPost() {
     }
   }, [selectedImage]);
 
+  // set a  function to handle the form submit when the user clicks the button
+  function handleSubmitForm(e){
+    e.preventDefault();
+    // create a reference for the image
+    const imageRef = ref(storage, `images/${formData.MediaUrl.name + v4()}`);
+    //upload to bucket
+    uploadBytes(imageRef, formData.MediaUrl).then((response)=>{
+      getDownloadURL(response.ref);
+    })
+    .catch((error)=> console.error(error));
+  }
+
+// handles the condtional rendering of JSX and also sets value for database
   const handleMediaUrl = (e) => {
     const optionValue = e.target.value;
     setMediaType(optionValue === 'Video');
     setFormData({...formData, PostType: e.target.value})
   };
 
-  const ImageUrlState = (e)=>{
-    setSelectedImage(e.target.files[0])
-    setFormData({...formData, MediaUrl: e.target.value})
-  }
+  // set the state of the on change for image 
+  const ImageUrlState = (e) => {
+    const selectedImage = e.target.files[0]; // Get the selected image
+    setSelectedImage(selectedImage);
+    setFormData({ ...formData, MediaUrl: selectedImage }); // Set the selected image as MediaUrl
+  };
+  // set the state of the on change for video link 
+  const videoUrlState = (e) => {
+    const videoLink = e.target.value; // Get the video link from the input field
+    setYoutubeLink(videoLink);
+    setFormData({ ...formData, MediaUrl: videoLink }); // Set the video link as MediaUrl
+  };
 
-    const videoUrlState = (e)=>{
-    setFormData({...formData, MediaUrl: e.target.value})
-    setFormData({...formData, MediaUrl: e.target.value})
-  }
+
 
 
   return (
     <div className='add-post-container'>
       <h2>Create A Post</h2>
       <div className='post-form-container'>
-        <form>
+        <form onSubmit={handleSubmitForm}>
           <label htmlFor="caption">Post Caption</label>
-          <textarea id='caption' placeholder='Write your caption here...' required onChange={(e)=>setFormData({...formData, Caption: e.target.value})}/>
+          <textarea id='caption' placeholder='Write your caption here...' onChange={(e)=>setFormData({...formData, Caption: e.target.value})}/>
 
           <label htmlFor="post-type">Post Type</label>
-          <select id="post-type" onChange={handleMediaUrl} required>
+          <select id="post-type" onChange={handleMediaUrl} >
             <option value="">Select</option>
             <option value="Video">Music</option>
             <option value="Picture">Picture</option>
@@ -61,7 +84,7 @@ function AddPost() {
                 type="text"
                 id='FileUrl'
                 onChange={videoUrlState}
-                required
+                
               />
               <div className='youtube-box'>
                 <ReactPlayer
@@ -69,7 +92,6 @@ function AddPost() {
                   controls={true}
                   className='video'
                   style={youtubeLink ? { display: 'block' } : { display: 'none' }}
-                  onChange={(e) => setYoutubeLink(e.target.value)}
                 />
               </div>
             </>
@@ -82,7 +104,7 @@ function AddPost() {
                 id='FileUrl'
                 accept='image/*'
                 onChange={ImageUrlState}
-                required
+                
               />
               <label htmlFor="FileUrl" className="custom-file-label" id="label-size">
                 {selectedImage ? selectedImage.name : 'Choose an Image'}
@@ -96,7 +118,7 @@ function AddPost() {
               )}
             </>
           )}
-          <button>Submit</button>
+          <button type='submit'>Submit</button>
         </form>
       </div>
     </div>
