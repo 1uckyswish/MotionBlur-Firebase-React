6a7ba1516cc0e05f20c757c1641ce25a8982c213
Noel Guillen <noelblas274@gmail.com>
2023-07-30 12:57:41 -0400

    comment post id added for specific comment

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -3,20 +3,35 @@ import { AiTwotoneLike } from 'react-icons/ai';
 import { AiOutlineLike } from 'react-icons/ai';
 import { db, auth } from '../../Config/FirebaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, where} from 'firebase/firestore';
 import { toast } from 'react-toastify';
 
-function Comments({ postId, commentState }) {
+
+
+function Comments({ postId, commentState, userName}) {
   const [commentLiked, setCommentLiked] = useState(false);
   const [user] = useAuthState(auth);
   const [commentDots, setCommentDots] = useState('.');
   const [newComment, setNewComment] = useState('');
+  const [arrayOfComments, setArrayOfComments] = useState([]);
 
   useEffect(
     ()=>{
+      //* get a reference to comments collection database
+       const commentsRef = collection(db, 'Comments');
 
+      //! filter to show only comments for one specific article
+      const q = query(commentsRef, where('PostId', '==', postId))
+
+      //* then use a promise to get the comments
+      getDocs(q,commentsRef).then((res)=>{
+        const comments = res.docs.map((item)=>({
+          ...item.data(),
+          id: item.id,
+        }));
+        setArrayOfComments(comments);
+      });
     },[])
-
 
 
   function addNewComment(e){
@@ -53,27 +68,26 @@ function Comments({ postId, commentState }) {
     <div>
         {
         user?
-        (commentState? (<form onSubmit={addNewComment}><input type="text" placeholder={`Leave Hippo a comment${commentDots}`} onChange={(e)=> setNewComment(e.target.value)} value={newComment}/> </form>) : (null))
+        (commentState? (<form onSubmit={addNewComment}><input type="text" placeholder={`Leave ${userName} a comment${commentDots}`} onChange={(e)=> setNewComment(e.target.value)} value={newComment}/> </form>) : (null))
         :
         (<p id="error-login">Please Login To Comment!</p>)
         }
-      <p>
-        <span>Hippo Jr:</span>
-        <span id="comment-section-like-icon">
-          hello hello hello hello hello hello hello hello hello
-          {commentLiked ? (
-            <AiTwotoneLike
-              id="liked-comment-icon"
-              onClick={() => setCommentLiked(!commentLiked)}
-            />
-          ) : (
-            <AiOutlineLike
-              id="unliked-comment-icon"
-              onClick={() => setCommentLiked(!commentLiked)}
-            />
-          )}
-        </span>
-      </p>
+         {
+          arrayOfComments.map((item)=>{
+            return (
+            <p key={item?.id}>
+            <span>{item.UserName}:</span>
+            <span id="comment-section-like-icon">{item.Content}
+            {
+            commentLiked?
+            (<AiTwotoneLike id="liked-comment-icon" onClick={() => setCommentLiked(!commentLiked)}/>)
+            :
+            (<AiOutlineLike id="unliked-comment-icon" onClick={() => setCommentLiked(!commentLiked)}/>)
+            }
+            </span>
+            </p>)
+          })
+        }
     </div>
   );
 }
