321b5e12497eb95f589f05f8895c91c413c4cbf6
Noel Guillen <noelblas274@gmail.com>
2023-08-06 15:52:12 -0400

    header media query completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,52 +1,61 @@
-//*import  components
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '/src/Config/FirebaseConfig';
-import {useAuthState} from 'react-firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { signOut } from 'firebase/auth';
 import './Header.css';
+import { Spiral as Hamburger } from 'hamburger-react';
 
 function Header() {
   const navigate = useNavigate();
   const [user] = useAuthState(auth);
+  const [isOpen, setOpen] = useState(false);
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
 
-  const logOut =()=>{
+  const logOut = () => {
     navigate('/');
     signOut(auth);
-  }
+  };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const showHamburger = viewportWidth <= 650;
 
   return (
     <header>
-        <h1 onClick={()=>navigate('/')}>Motion</h1>
-        <div className={user? 'user-trending-hashtags' : 'trending-hashtags'}>
-          <p onClick={()=>navigate('/Trending')}>Trending</p>
-          <p onClick={()=>navigate('/MusicPage')}>Music</p>
-          <p onClick={()=>navigate('/TimeLine')}>Photos</p>
-          {
-            user?
-             <p onClick={()=>navigate('/AddPost')}>Create Post</p>
-             :
-            null
-          }
+      <h1 onClick={() => navigate('/')}>Motion</h1>
+      <div className={user ? 'user-trending-hashtags' : 'trending-hashtags'}>
+        <p onClick={() => navigate('/Trending')}>Trending</p>
+        <p onClick={() => navigate('/MusicPage')}>Music</p>
+        <p onClick={() => navigate('/TimeLine')}>Photos</p>
+        {user ? <p onClick={() => navigate('/AddPost')}>Create Post</p> : null}
+      </div>
+      {user ? (
+        <div className='profile-signout-box'>
+          {user.photoURL ? (
+            <img onClick={() => navigate(`/ProfileAccount/${user?.uid}`)} src={user.photoURL} />
+          ) : (
+            <img
+              onClick={() => navigate(`/ProfileAccount/${user?.uid}`)}
+              src='https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png'
+            />
+          )}
+          <button onClick={logOut}>Log Out</button>
         </div>
-        {
-          user?
-          <div className='profile-signout-box'>
-            {
-              user.photoURL?
-              <img onClick={()=>navigate(`/ProfileAccount/${user?.uid}`)} src={user.photoURL}/>
-              :
-              <span onClick={()=>navigate(`/ProfileAccount/${user?.uid}`)}>{user?.displayName ? user?.displayName : user?.email}</span>
-            }
-            {/* <span onClick={()=>navigate('/ProfileAccount')}>{user?.displayName ? user?.displayName : user?.email}</span> */}
-             <button onClick={logOut}>Log Out</button>
-          </div>
-          :
-           <button onClick={()=>navigate('/SignUp')}>Sign Up</button>
-        }
+      ) : (
+        <button onClick={() => navigate('/SignUp')}>Sign Up</button>
+      )}
+      {showHamburger && <Hamburger toggled={isOpen} toggle={setOpen} />}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
