87fbfa5fb378a5977c2a21e2ae1e13fcddcce3b6
Noel Guillen <noelblas274@gmail.com>
2023-08-06 14:03:13 -0400

    fixed post error

--- src/Components/AdGrid/AdGrid.jsx
+++ src/Components/AdGrid/AdGrid.jsx
@@ -19,15 +19,15 @@ function AdGrid() {
           <p className="hidden-p">Unleash your imagination, find inspiration, and ignite conversations that transcend boundaries.</p>
         </div>
         <div className="ad-box ad-box-4">
-          <h2 className="hidden-h2">Follow & Like</h2>
+          <h2 className="hidden-h2">Like & Comment</h2>
           <p className="hidden-p">Ignite conversations with captivating content. Leave your mark through thought-provoking visuals and heart-pounding beats.</p>
         </div>
         <div className="ad-box ad-box-5">
-          <h2 className="hidden-h2">Comment & Share</h2>
+          <h2 className="hidden-h2">Share Posts</h2>
           <p className="hidden-p">Unleash your voice! Comment, share, and make an impact. Join the conversation and spread your influence.</p>
         </div>
         <div className="ad-box ad-box-6">
-          <h2 className="hidden-h2">Trending Topics</h2>
+          <h2 className="hidden-h2">Trending Posts</h2>
           <p className="hidden-p">Stay in the loop! Explore trending topics and be part of the buzz. Join the conversation and discover what's hot right now.</p>
         </div>
       </div>
