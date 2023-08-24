422dfa7012e223f47308feee3c95a62f525dba52
Noel Guillen <noelblas274@gmail.com>
2023-08-23 21:51:32 -0400

    footer links for socials applied

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 import "./Footer.css"
 //Import components
 import { AiFillGithub, AiFillLinkedin, AiFillInstagram} from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer>
         <div className='icon-container'>
-        <AiFillLinkedin />
-        <AiFillGithub/>
-        <AiFillInstagram />
+        <Link to="https://www.linkedin.com/in/noel-guillen-blas-b63353257/?trk=people-guest_people_search-card"><AiFillLinkedin /></Link>
+        <Link to="https://github.com/1uckyswish"><AiFillGithub/></Link>
+         <Link to="https://www.instagram.com/luckideveloper/"><AiFillInstagram /></Link>
         </div>
       <h1>Motion</h1>
       <div className='copyright-container'>
