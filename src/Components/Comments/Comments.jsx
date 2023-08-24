8d9d89e3fc5220f8508309a14a06395dd4633b21
Noel Guillen <noelblas274@gmail.com>
2023-08-23 20:36:36 -0400

    post details css fixed for responsive view

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,16 +1,14 @@
-import { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiTwotoneLike } from 'react-icons/ai';
 import { AiOutlineLike } from 'react-icons/ai';
 import { db, auth } from '../../Config/FirebaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { collection, addDoc, getDocs, query, where, onSnapshot, deleteDoc, doc } from 'firebase/firestore';
+import { collection, addDoc, query, where, onSnapshot, deleteDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import "./Comments.css"
 import { useNavigate } from 'react-router-dom';
 
-
-
-function Comments({ postId, commentState, userName}) {
+function Comments({ postId, commentState, userName }) {
   const [commentLiked, setCommentLiked] = useState(false);
   const [user] = useAuthState(auth);
   const [commentDots, setCommentDots] = useState('.');
@@ -18,28 +16,21 @@ function Comments({ postId, commentState, userName}) {
   const [arrayOfComments, setArrayOfComments] = useState([]);
   const nav = useNavigate();
 
-  useEffect(
-    ()=>{
-      //* get a reference to comments collection database
-       const commentsRef = collection(db, 'Comments');
+  useEffect(() => {
+    const commentsRef = collection(db, 'Comments');
+    const q = query(commentsRef, where('PostId', '==', postId));
+    onSnapshot(q, (snapshot) => {
+      const comments = snapshot.docs.map((item) => ({
+        ...item.data(),
+        id: item.id,
+      }));
+      setArrayOfComments(comments);
+    });
+  }, []);
 
-      //! filter to show only comments for one specific article
-      const q = query(commentsRef, where('PostId', '==', postId))
-
-      //* then use a promise to get the comments
-      onSnapshot(q,(snapshot)=>{
-        const comments = snapshot.docs.map((item)=>({
-          ...item.data(),
-          id: item.id,
-        }));
-        setArrayOfComments(comments);
-      });
-    },[])
-
-
-  function addNewComment(e){
+  const addNewComment = (e) => {
     e.preventDefault();
-     if (newComment.length <= 1) {
+    if (newComment.length <= 1) {
       toast.error('Provide a valid input', {
         position: "bottom-center",
         autoClose: 1000,
@@ -49,81 +40,79 @@ function Comments({ postId, commentState, userName}) {
         draggable: true,
         progress: undefined,
         theme: "dark",
-        });
-    // You can show an error message here, or prevent the comment from being posted
-    return;
-  }
-    //need to make a document in comments collection in the db
-    //include the newComment,  the post id and the user who added the comment
-    // create a reference to the Comments
-    // will create a collection if it doe not exist
+      });
+      return;
+    }
     const commentsRef = collection(db, 'Comments');
-    // adding a document with all the data
     addDoc(commentsRef, {
-        UserId: user?.uid,
-        PostId: postId,
-        Content: newComment,
-        UserName: user?.displayName,
+      UserId: user?.uid,
+      PostId: postId,
+      Content: newComment,
+      UserName: user?.displayName,
     })
-    .then(res=>{
-         toast("Comment Posted!", {
+      .then(res => {
+        toast("Comment Posted!", {
           type: "success",
           autoClose: 1000,
         });
         setNewComment('');
-    }).catch((err)=> console.log(err));
-  }
+      })
+      .catch((err) => console.log(err));
+  };
 
-  const deleteComment = id =>{
-    //* get the specific id of the post
+  const deleteComment = (id) => {
     deleteDoc(doc(db, 'Comments', id))
-    .then((success)=>{
-      toast.info("Comment Deleted!", {
+      .then((success) => {
+        toast.info("Comment Deleted!", {
           autoClose: 1000,
         });
-    })
-    .catch((error)=>{
+      })
+      .catch((error) => {
+        // Handle error here
+      });
+  };
 
-    })
-  }
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      addNewComment(e);
+    }
+  };
 
-   setTimeout(() => {
-      setCommentDots(commentDots + ".")
-      if(commentDots.length >= 3){
-        setCommentDots(".")
-      }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCommentDots((prevDots) => prevDots.length >= 3 ? "." : prevDots + ".");
     }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-        {
-        user?
-        (commentState? (<form onSubmit={addNewComment}><input type="text" placeholder={`Leave ${userName} a comment${commentDots}`} maxLength="85"  onChange={(e)=> setNewComment(e.target.value)} value={newComment}/> </form>) : (null))
-        :
-        (<p id="error-login">Please <span id="error-login-text" onClick={()=>nav('/SignUp')}>Login</span> To Comment!</p>)
-        }
-         {
-          arrayOfComments.map((item)=>{
-            return (
-            <div key={item?.id}>
-            <span onClick={()=>nav(`/ProfileAccount/${item?.UserId}`)}>{item.UserName}:</span>
-            <span id="comment-section-like-icon">{item.Content}
-            {
-              // comment has a uid
-              // compare to see if I can delete a Comment
-              user?.uid === item?.UserId?
-              (<button id="comment-delete" onClick={()=>deleteComment(item.id)}>Delete</button>)
-              :
-              null
-              // commentLiked?
-              // (<AiTwotoneLike id="liked-comment-icon" onClick={() => setCommentLiked(!commentLiked)} />)
-              // :
-              // (<AiOutlineLike id="unliked-comment-icon" onClick={() => setCommentLiked(!commentLiked)} />)
-            }
-            </span>
-            </div>)
-          })
-        }
+      {user ?
+        (commentState ? (
+          <form onSubmit={addNewComment}>
+            <textarea
+              type="text"
+              placeholder={`Leave ${userName} a comment${commentDots}`}
+              onChange={(e) => setNewComment(e.target.value)}
+              onKeyDown={handleKeyPress}
+              value={newComment}
+            />
+          </form>
+        ) : (null))
+        : (<p id="error-login">Please <span id="error-login-text" onClick={() => nav('/SignUp')}>Login</span> To Comment!</p>)
+      }
+      {arrayOfComments.map((item) => (
+        <div key={item?.id}>
+          <span onClick={() => nav(`/ProfileAccount/${item?.UserId}`)}>{item.UserName} :</span>
+          <span id="comment-section-like-icon"><p>{item.Content}</p>
+            {user?.uid === item?.UserId ? (
+              <button id="comment-delete" onClick={() => deleteComment(item.id)}>Delete</button>
+            ) : null}
+          </span>
+        </div>
+      ))}
     </>
   );
 }
