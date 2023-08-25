72085481ab4b3b8be23bee5693a10b5994829718
Noel Guillen <noelblas274@gmail.com>
2023-08-24 21:43:48 -0400

    added images to object to map for every image

--- src/Components/AdGrid/AdGrid.jsx
+++ src/Components/AdGrid/AdGrid.jsx
@@ -1,37 +1,48 @@
 import {useState, useEffect} from 'react';
 import Grid1 from "../../assets/GridImgs/image1.webp";
+import Grid2 from "../../assets/GridImgs/image2.webp";
+import Grid3 from "../../assets/GridImgs/image3.webp";
+import Grid4 from "../../assets/GridImgs/image4.webp";
+import Grid5 from "../../assets/GridImgs/image5.webp";
+import Grid6 from "../../assets/GridImgs/image6.webp";
 import "./AdGrid.css";
 
 const adData = [
   {
     title: "About",
     description: "Unleash your creativity! Share passions through captivating visuals and soulful music. Experience art and sound seamlessly. Join us today!",
-    className: "ad-box-1"
+    className: "ad-box-1",
+    Image: Grid1,
   },
   {
     title: "Discover",
     description: "Vibrant community of artists, photographers, music lovers, and dreamers.",
-    className: "ad-box-2"
+    className: "ad-box-2",
+    Image: Grid2,
   },
   {
     title: "Collaborate",
     description: "Unleash your imagination, find inspiration, and ignite conversations that transcend boundaries.",
-    className: "ad-box-3"
+    className: "ad-box-3",
+    Image: Grid3,
   },
   {
     title: "Like & Comment",
     description: "Ignite conversations with captivating content. Leave your mark through thought-provoking visuals and heart-pounding beats.",
-    className: "ad-box-4"
+    className: "ad-box-4",
+    Image: Grid4,
   },
   {
     title: "Share Posts",
     description: "Unleash your voice! Comment, share, and make an impact. Spread your influence through conversation.",
-    className: "ad-box-5"
+    className: "ad-box-5",
+    Image: Grid5,
   },
   {
     title: "Trending Posts",
     description: "Stay in the loop! Explore trending topics and be part of the buzz.",
-    className: "ad-box-6"
+    className: "ad-box-6",
+    Image: Grid6,
   }
 ];
 
@@ -48,16 +59,19 @@ function AdGrid() {
 
   return (
     <div className="container">
-      {adData.map((ad, index) => (
-        <div
-          key={index}
-          className={`ad-box ${ad.className} ${index === hoverIndex ? 'active' : ''}`}
-        >
-          <h2 className={`hidden-h2 ${index === hoverIndex ? 'active' : ''}`}>{ad.title}</h2>
-          <p className={`hidden-p ${index === hoverIndex ? 'active' : ''}`}>{ad.description}</p>
-        </div>
-      ))}
-    </div>
+    {adData.map((ad, index) => (
+      <div
+        key={index}
+        className={`ad-box ${ad.className} ${index === hoverIndex ? 'active' : ''}`}
+        style={{
+          backgroundImage: `url(${ad.Image})`, // Set the background image unconditionally
+        }}
+      >
+        <h2 className={`hidden-h2 ${index === hoverIndex ? 'active' : ''}`}>{ad.title}</h2>
+        <p className={`hidden-p ${index === hoverIndex ? 'active' : ''}`}>{ad.description}</p>
+      </div>
+    ))}
+  </div>
   );
 }
 
